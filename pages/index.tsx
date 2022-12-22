c3113cbfa64753cf1a90632cd96951952ef3e46c
Alice <jaeyeon0921@gmail.com>
2022-12-22 16:29:54 +0900

    Apply layout to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,12 @@
-import Head from "next/head";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Layout from "../components/Layout";
 import CarouselLarge from "../components/Carousel/CarouselLarge";
 import QuickMenu from "../components/QuickMenu";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Buy Book</title>
-        <meta name="description" content="Created by Alice and Spes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <main className="h-full mx-8">
-        <CarouselLarge />
-        <QuickMenu />
-      </main>
-      <Footer />
-    </>
+    <Layout title="buyBook">
+      <CarouselLarge />
+      <QuickMenu />
+    </Layout>
   );
 }
