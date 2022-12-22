f8783565922056044eb39c4ee44e66bd91d9b112
Alice <jaeyeon0921@gmail.com>
2022-12-22 16:29:41 +0900

    Apply login and logout

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,37 +6,51 @@ import {
   AiOutlineSearch,
 } from "react-icons/ai";
 import { ImBooks } from "react-icons/im";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <div className="h-full mx-8">
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">BuyBook</a>
+          <Link href="/" className="btn btn-ghost normal-case text-xl">
+            BuyBook
+          </Link>
         </div>
-        <div className="flex flex-row gap-4">
-          <div className="form-control ">
-            <AiOutlineSearch className="absolute h-[20px] w-[20px] my-[10px] mx-[10px]" />
-            <input
-              type="text"
-              placeholder=""
-              className="input  bg-main-grey w-[260px] h-[40px]"
-            />
+        {session ? (
+          <div className="flex flex-row gap-4">
+            <div className="form-control ">
+              <AiOutlineSearch className="absolute h-[20px] w-[20px] my-[10px] mx-[10px]" />
+              <input
+                type="text"
+                placeholder=""
+                className="input  bg-main-grey w-[260px] h-[40px]"
+              />
+            </div>
+            <div>
+              <AiOutlineBell className="h-[24px] w-[24px] fill-dark-grey" />
+            </div>
+            <div>
+              <AiOutlineShoppingCart className="h-[24px] w-[24px] fill-dark-grey" />
+            </div>
+            <div>
+              <ImBooks className="h-[24px] w-[24px] fill-dark-grey" />
+            </div>
+            <div>
+              <AiOutlineUser className="h-[24px] w-[24px] fill-dark-grey" />
+            </div>
+            <button onClick={() => signOut()}>로그아웃</button>
           </div>
-          <div>
-            <AiOutlineBell className="h-[24px] w-[24px] fill-dark-grey" />
+        ) : (
+          <div className="flex flex-row gap-4">
+            <Link href="/signup">회원가입</Link>
+            <button onClick={() => signIn()}>로그인</button>
           </div>
-          <div>
-            <AiOutlineShoppingCart className="h-[24px] w-[24px] fill-dark-grey" />
-          </div>
-          <div>
-            <ImBooks className="h-[24px] w-[24px] fill-dark-grey" />
-          </div>
-          <div>
-            <AiOutlineUser className="h-[24px] w-[24px] fill-dark-grey" />
-          </div>
-        </div>
+        )}
       </div>
+      <hr className="py-3" />
     </div>
   );
 }
